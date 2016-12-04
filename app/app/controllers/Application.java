9de61d52b4931bd4a06b011c0f8f2f6ed01e8bcb
anais-mezioune <anais.mezioune@gmail.com>
2016-12-04 23:49:08 +0100

    Nettoyage

--- app/app/controllers/Application.java
+++ app/app/controllers/Application.java
@@ -12,37 +12,37 @@ import java.util.*;
 import models.*;
 
 public class Application extends Controller {
-	
+
 	public static void index() {
     	List<Pays> listePays = Pays.findAll();
         List<Annee> listeAnnees = AnneeService.listAnneesOrdonnee();
         render(listePays, listeAnnees/*, paysDonnees*/);
     }
-    
+
 	/**
 	 * @author Anaïs
 	 * @param pays
 	 * @param annees
 	 * @param critere
 	 */
-	 public static <T> void confirmer(String pays, String annees, String critere) {
+		public static <T> void confirmer(String pays, String annees, String critere) {
 	    	String[] tabPays;
-	    	String[] tabAnnees; 
+	    	String[] tabAnnees;
 	    	System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>pays : "+ pays);
 	    	System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>annees : "+ annees);
-	    	
+
 	    	if (pays.indexOf(',') != -1){
 	    		tabPays = pays.split(",");
 	    	} else{
 	    		tabPays = PaysDonneesService.stringToArray(pays);
 	    	}
-	    	
+
 	    	if (annees.indexOf(',') != -1){
 	    		tabAnnees = annees.split(",");
 	    	} else{
 	    		tabAnnees = PaysDonneesService.stringToArray(annees);
 	    	}
-	    	
+
 	    	ArrayList<ArrayList> donnees = new ArrayList();
 	    	for(int j = 0 ; j < tabAnnees.length ; j++){
 	    		ArrayList<T> donnee = new ArrayList();
@@ -55,7 +55,7 @@ public class Application extends Controller {
 	    	renderJSON(donnees);
 //	    	 String data = "[[\"2010\", 23, 17], [\"2011\", 17, 23]]";
 	    }
-	 
+
     /**
      * @author Anaïs
      * @param pays
@@ -64,29 +64,29 @@ public class Application extends Controller {
      */
     public static void confirmer2(String pays, String annees, String critere) {
     	String[] tabPays;
-    	String[] tabAnnees; 
+    	String[] tabAnnees;
     	String[] tabCriteresRecherche;
     	System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>pays : "+ pays);
     	System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>annees : "+ annees);
-    	
+
     	if (pays.indexOf(',') != -1){
     		tabPays = pays.split(",");
     	} else{
     		tabPays = PaysDonneesService.stringToArray(pays);
     	}
-    	
+
     	if (annees.indexOf(',') != -1){
     		tabAnnees = annees.split(",");
     	} else{
     		tabAnnees = PaysDonneesService.stringToArray(annees);
     	}
-    	
+
     	String donnees = "[";
     	for(int j = 0 ; j < tabAnnees.length ; j++){
 			donnees += "[\"" + tabAnnees[j]  + "\",";
 			for(int k = 0 ; k < tabPays.length ; k++){
 		    		donnees += PaysDonneesService.chercherDonneesPaysAnneeByCritere(critere, tabPays[k], tabAnnees[j]);
-				
+
 				if(k == tabPays.length-1){
 					break;
 				}else{
@@ -99,7 +99,7 @@ public class Application extends Controller {
 				donnees += ",";
     		}
 		}
-    	donnees += "]"; 
+    	donnees += "]";
     	renderJSON(donnees);
 //    	 String data = "[[\"2010\", 23, 17], [\"2011\", 17, 23]]";
     }
